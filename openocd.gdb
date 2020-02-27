5a21a0acd4392de76c2dda0ff03bb0f41e5e8829
Henk Dieter <henkdieter@tweedegolf.com>
2020-02-27 15:42:49 +0100

    WIP on getting PCA to work

--- openocd.gdb
+++ openocd.gdb
@@ -4,12 +4,12 @@ target extended-remote :3333
 set print asm-demangle on
 
 # detect unhandled exceptions, hard faults and panics
-break DefaultHandler
-break UserHardFault
+# break DefaultHandler
+# break UserHardFault
 break rust_begin_unwind
 
 # *try* to stop at the user entry point (it might be gone due to inlining)
-break main
+
 
 monitor arm semihosting enable
 
@@ -28,4 +28,4 @@ monitor arm semihosting enable
 load
 
 # start the process but immediately halt the processor
-stepi
+continue
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,12 @@
 // Import a directive which marks the entrypoint of the application.
 use cortex_m_rt::entry;
 
-// Import the embedded_hal trait implementations for the STM32F30x.
-use stm32f30x_hal::prelude::*;
-// We need to explicitly import the v2 OutputPin trait.
+use core::convert::Infallible;
 use embedded_hal::digital::v2::OutputPin;
-// No means No-Op. It's an operation which does nothing.
-// We use it in the busy waiting loops to notify Rust that it
-// should not optimize these loops out.
-use cortex_m::asm::nop;
+use stm32f30x_hal::prelude::*;
+use stm32f30x_hal::time::MegaHertz;
+
+const PCA_I2C_ADDR:u8 = 0x80;
 
 // A panic handler is run when the application encounters an error
 // it cannot recover from. The handler defines what it should do
@@ -24,44 +22,125 @@ unsafe fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(debug_assertions)]
+fn busy_wait(ms: u32) {
+    use cortex_m::asm::nop;
+    
+    for _ in 0..(ms * 70) {
+        nop();
+    }
+}
+#[cfg(not(debug_assertions))]
+fn busy_wait(ms: u32) {
+    use cortex_m::asm::nop;
+    
+    for _ in 0..(ms * 1400) {
+        nop();
+    }
+}
+
+fn blink_led(led: &mut impl OutputPin<Error = Infallible>, times: u16) -> Result<(), Infallible> {
+    for _ in 0..times {
+        led.set_high()?;
+        busy_wait(100);
+        led.set_low()?;
+        busy_wait(100);
+    }
+    Ok(())
+}
+
 // This is the main function, or entrypoint of our applicaton.
 #[entry]
 fn main() -> ! {
-    // Get a handle to the peripherals. Safe Rust allows only a single instance
-    // of this handle. That way, accidental concurrent access is avoided.
+    // Get a handle to the peripherals
     let peripherals = stm32f30x::Peripherals::take().unwrap();
 
-    // Reset and clock control register. Among other things, this register
-    // is for enabling the General Purpose Input/Output peripherals.
-    // We constrain full access to the RCC, allowing access per part instead.
-    // Individual modules can configure individual parts of the RCC independently
-    // from now on. This gets more important in larger applications.
+    // Reset and clock control register
     let mut rcc = peripherals.RCC.constrain();
 
-    // The compass LEDs are all connected to the GPIO E peripheral.
-    // Splitting the GPIO provides access to each of the individual pins,
-    // so we can configure each of them independently.
+    let mut flash = peripherals.FLASH.constrain();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut gpiob = peripherals.GPIOB.split(&mut rcc.ahb);
     let mut gpioe = peripherals.GPIOE.split(&mut rcc.ahb);
-    
-    // The Northern LED is connected to pin pe9. To use it four our purpose,
-    // we configure it to a push-pull output.
-    let mut led = gpioe
+
+    let mut led_n = gpioe
         .pe9
         .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led_ne = gpioe
+        .pe10
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led_e = gpioe
+        .pe11
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led_se = gpioe
+        .pe12
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led_s = gpioe
+        .pe13
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led_sw = gpioe
+        .pe14
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led_w = gpioe
+        .pe15
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led_nw = gpioe
+        .pe8
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+
+    let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+    let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+    let mut i2c1 = stm32f30x_hal::i2c::I2c::i2c1(
+        peripherals.I2C1,
+        (scl, sda),
+        MegaHertz(1),
+        clocks,
+        &mut rcc.apb1,
+    );
+    blink_led(&mut led_w, 10).unwrap();
+
+    busy_wait(100);
+    i2c1.write(PCA_I2C_ADDR, &[0x00, 0x00]).unwrap();
+    blink_led(&mut led_sw, 10).unwrap();
+    busy_wait(100);
+    i2c1.write(PCA_I2C_ADDR, &[0x01, 0x00]).unwrap();
+    blink_led(&mut led_nw, 10).unwrap();
+
+    blink_led(&mut led_s, 2).unwrap();
+    match i2c1.write(PCA_I2C_ADDR, &[0x42, 0xFF, 0x0F]) {
+        Err(_) => blink_led(&mut led_n, 2).unwrap(),
+        Ok(_) => blink_led(&mut led_e, 2).unwrap(),
+    }
+    
+    blink_led(&mut led_s, 2).unwrap();
+    match i2c1.write(PCA_I2C_ADDR, &[0x43,  0xFF, 0x0F]) {
+        Err(_) => blink_led(&mut led_n, 2).unwrap(),
+        Ok(_) => blink_led(&mut led_e, 2).unwrap(),
+    }
+    
+    blink_led(&mut led_s, 2).unwrap();
+    match i2c1.write(PCA_I2C_ADDR, &[0x44,  0x00, 0x00, 0x00]) {
+        Err(_) => blink_led(&mut led_n, 2).unwrap(),
+        Ok(_) => blink_led(&mut led_e, 2).unwrap(),
+    }
+    
+    blink_led(&mut led_s, 2).unwrap();
+    match i2c1.write(PCA_I2C_ADDR, &[0x45,  0x00, 0x00, 0x00]) {
+        Err(_) => blink_led(&mut led_n, 2).unwrap(),
+        Ok(_) => blink_led(&mut led_e, 2).unwrap(),
+    }
+    
     
     // Loop forever
     loop {
-        // Wait a couple of cycles
-        for _ in 0..100_000 {
-            nop();
-        }
-        // Enable the LED
-        led.set_high().unwrap();
-        // Wait some more
-        for _ in 0..100_000 {
-            nop();
-        }
-        // Disable the LED
-        led.set_low().unwrap();
+        blink_led(&mut led_n, 1).unwrap();
+        blink_led(&mut led_ne, 1).unwrap();
+        blink_led(&mut led_e, 1).unwrap();
+        blink_led(&mut led_se, 1).unwrap();
+        blink_led(&mut led_s, 1).unwrap();
+        blink_led(&mut led_sw, 1).unwrap();
+        blink_led(&mut led_w, 1).unwrap();
+        blink_led(&mut led_nw, 1).unwrap();
     }
 }
